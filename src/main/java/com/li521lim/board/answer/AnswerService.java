deeaea6bde8b71625648b14dcbc767d02e170ffd
LiS2Lim <s2alua.fu@gmail.com>
2023-06-26 11:56:31 +0900

    第02_11_コメント登録

---
+++ src/main/java/com/li521lim/board/answer/AnswerService.java
@@ -0,0 +1,24 @@
+package com.li521lim.board.answer;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.li521lim.board.question.Question;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+	private final AnswerRepository answerRepository;
+	
+	public void create(Question question, String content) {
+		Answer answer = new Answer();
+		answer.setContent(content);
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setQuestion(question);
+		this.answerRepository.save(answer);
+	}
+}
